2253c909ef13cfa7e19d36c4227c9a2e1cb406ba
hejian <hejian@xiaoantech.com>
2021-06-05 15:33:29 +0800

    ADD init

---
+++ src/main/java/com/hj/swagger/web/NodeClient.java
@@ -0,0 +1,11 @@
+package com.hj.swagger.web;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "nodejs")
+public interface NodeClient {
+
+    @GetMapping("/hello")
+    String hello();
+}
